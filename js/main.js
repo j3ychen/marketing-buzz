93467de605eaa371691dc597537dbc339bb6f855
Jerry Chen <jychen216@gmail.com>
2015-11-13 12:40:50 -0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 function makePhrase() {
   var words1 = ["Let's", "It's time to", "We have to"];
-  var words2 = ["synergize", "revolutionize", "re-think", "streamline", "optimize", "disrupt", "pinpoint", "re-define"
-    "leverage", "power through", "scale"];
-  var words3 = ["win-win", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", 
-    "glocalization", "go-to-market", "value-add", "empowerment", "diversity"];
-  var words4 = ["paradigm", "strategy", "vision", "process", "potential", "algorithm", "framework", "bandwidth",
-    "ecosystem"];
+  var words2 = ["synergize", "revolutionize", "re-think", "streamline", "optimize", "disrupt", "pinpoint", "re-define", "leverage", "power through", "scale"];
+  var words3 = ["win-win", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", "glocalization", "go-to-market", "value-add", "empowerment", "diversity"];
+  var words4 = ["paradigm", "strategy", "vision", "process", "potential", "algorithm", "framework", "bandwidth", "ecosystem"];
 
   var rand1 = Math.floor(Math.random() * words1.length);
   var rand2 = Math.floor(Math.random() * words2.length);
