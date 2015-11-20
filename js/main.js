4810912c906a365e995a8276577aa1ff6df91d7e
Jerry Chen <jychen216@gmail.com>
2015-11-19 17:52:34 -0800

    added more buzz words

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function makePhrase() {
   var words1 = ["Let's", "It's time to", "We have to"];
-  var words2 = ["synergize", "revolutionize", "re-think", "streamline", "optimize", "disrupt", "pinpoint", "re-define", "leverage", "re-invent", "scale", "monetize", "re-imagine"];
-  var words3 = ["win-win", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", "glocalization", "go-to-market", "value-add", "empowerment", "upcycling"];
-  var words4 = ["paradigm", "strategy", "vision", "process", "potential", "algorithm", "framework", "bandwidth", "ecosystem", "culture", "infrastructure"];
+  var words2 = ["synergize", "revolutionize", "re-think", "streamline", "optimize", "disrupt", "pinpoint", "re-define", "leverage", "re-invent", "scale", "monetize", "re-imagine", "prioritize", "engage", "focus on", "finetune", "automate", "offshore", "integrate", "tee off"];
+  var words3 = ["win-win", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", "glocalization", "go-to-market", "value-add", "empowerment", "upcycling", "modularization", "accountability", "technology-enabled", "next generation"];
+  var words4 = ["paradigm", "strategy", "vision", "process", "potential", "algorithm", "framework", "bandwidth", "ecosystem", "culture", "infrastructure", "capacity", "bizmeth", "impact", "advantage"];
 
   var rand1 = Math.floor(Math.random() * words1.length);
   var rand2 = Math.floor(Math.random() * words2.length);
