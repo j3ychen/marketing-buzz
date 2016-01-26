610d71dde429dfa2be7291153f42b360f7759743
Jerry Chen <jychen216@gmail.com>
2016-01-25 20:34:46 -0800

    Move phrase up a little

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function makePhrase() {
   var words1 = ["Let's", "It's time to", "We have to"];
   var words2 = ["synergize", "revolutionize", "re-think", "streamline", "optimize", "disrupt", "pinpoint", "re-define", "leverage", "re-invent", "scale", "monetize", "re-imagine", "prioritize", "engage", "focus on", "finetune", "automate", "offshore", "integrate", "tee off"];
-  var words3 = ["win-win", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", "glocalization", "go-to-market", "value-add", "empowerment", "upcycling", "modularization", "accountability", "technology-enabled", "next generation"];
+  var words3 = ["win-win", "proprietary", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", "glocalization", "go-to-market", "value-add", "empowerment", "upcycling", "modularization", "accountability", "technology-enabled", "next generation"];
   var words4 = ["paradigm", "strategy", "vision", "process", "potential", "algorithm", "framework", "bandwidth", "ecosystem", "culture", "infrastructure", "capacity", "bizmeth", "impact", "advantage"];
 
   var rand1 = Math.floor(Math.random() * words1.length);
