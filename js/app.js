6899fc47398eb9513d1e246caeb9391e5414c15b
Jerry Chen <jychen216@gmail.com>
2016-08-14 13:09:50 -0700

    Fix ng-click function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,20 @@
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function($scope) {
-//    $scope.w1 = w1;
+    $scope.w1 = document.getElementById("w1");
+    $scope.w2 = document.getElementById("w2");
+    $scope.w3 = document.getElementById("w3");
+    $scope.w4 = document.getElementById("w4");
+    
     $scope.generate = function(x) {
-    	$scope.x = x;
-    	makePhrase();
+    	if (x=='w3') {
+    		keepW3();
+    	} else if (x=='w2') {
+    		keepW2();
+    	} else {
+    		keepW4();
+    	}
+    }
+    $scope.highlight = function() {
+    	$scope.w2 = {color: red};
     }
 });
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function makePhrase() {
   var words1 = ["Let's", "It's time to", "We have to"];
   var words2 = ["synergize", "revolutionize", "re-think", "streamline", "optimize", "disrupt", "pinpoint", "re-define", "leverage", "re-invent", "scale", "monetize", "re-imagine", "prioritize", "engage", "focus on", "finetune", "automate", "offshore", "integrate", "tee off"];
   var words3 = ["win-win", "proprietary", "vertical-horizontal", "sustainability", "big data", "innovation", "ideation", "growth", "glocalization", "go-to-market", "value-add", "empowerment", "upcycling", "modularization", "accountability", "technology-enabled", "next generation"];
   var words4 = ["paradigm", "strategy", "vision", "process", "potential", "algorithm", "framework", "bandwidth", "ecosystem", "culture", "infrastructure", "capacity", "bizmeth", "impact", "advantage"];
 
+function makePhrase() {
   var rand1 = Math.floor(Math.random() * words1.length);
   var rand2 = Math.floor(Math.random() * words2.length);
   var rand3 = Math.floor(Math.random() * words3.length);
@@ -11,15 +11,43 @@ function makePhrase() {
 
 //  var phrase = words1[rand1] + " " + words2[rand2] + " our " + words3[rand3] + " " + words4[rand4] + ".";
 //  var phraseElement = document.getElementById("phrase");
+//  phraseElement.innerHTML = phrase;
+
   var w1 = document.getElementById("w1");
   var w2 = document.getElementById("w2");
   var w3 = document.getElementById("w3");
   var w4 = document.getElementById("w4");
-//  phraseElement.innerHTML = phrase;
   w1.innerHTML = words1[rand1];
   w2.innerHTML = words2[rand2];
   w3.innerHTML = words3[rand3];
   w4.innerHTML = words4[rand4];
+}
+
+function keepW3() {
+  var w1 = document.getElementById("w1");
+  var w2 = document.getElementById("w2");
+  var w3 = document.getElementById("w3");
+  w1.innerHTML = words1[Math.floor(Math.random() * words1.length)];
+  w2.innerHTML = words2[Math.floor(Math.random() * words2.length)];
+  w4.innerHTML = words4[Math.floor(Math.random() * words4.length)];
+}
+
+function keepW4() {
+  var w1 = document.getElementById("w1");
+  var w2 = document.getElementById("w2");
+  var w3 = document.getElementById("w3");
+  w1.innerHTML = words1[Math.floor(Math.random() * words1.length)];
+  w2.innerHTML = words2[Math.floor(Math.random() * words2.length)];
+  w3.innerHTML = words3[Math.floor(Math.random() * words3.length)];
+}
+
+function keepW2() {
+  var w1 = document.getElementById("w1");
+  var w2 = document.getElementById("w2");
+  var w4 = document.getElementById("w4");
+  w1.innerHTML = words1[Math.floor(Math.random() * words1.length)];
+  w3.innerHTML = words3[Math.floor(Math.random() * words3.length)];
+  w4.innerHTML = words4[Math.floor(Math.random() * words4.length)];
 }
 
 window.onload = makePhrase;
